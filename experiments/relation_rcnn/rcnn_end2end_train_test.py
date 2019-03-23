0a32c00c0752fee5ece395ad5f25c4ae588892df
insigh <zhangchaojie@ruc.edu.cn>
2019-03-23 15:39:42 +0800

    add me as modifier

--- experiments/relation_rcnn/rcnn_end2end_train_test.py
+++ experiments/relation_rcnn/rcnn_end2end_train_test.py
@@ -2,7 +2,7 @@
 # Relation Networks for Object Detection
 # Copyright (c) 2017 Microsoft
 # Licensed under The MIT License [see LICENSE for details]
-# Written by Jiayuan Gu, Dazhi Cheng, Guodong Zhang
+# Written by Jiayuan Gu, Dazhi Cheng, Guodong Zhang  Chaojie
 # --------------------------------------------------------
 
 import os
