8c71fd4cbb25cd965fd40b3fad03c1cd1c0dac14
insigh <zhangchaojie@ruc.edu.cn>
2019-04-04 20:56:47 +0800

    generate rpn proposal file debug

--- relation_rcnn/train_rpn.py
+++ relation_rcnn/train_rpn.py
@@ -11,6 +11,17 @@
 # https://github.com/ijkguo/mx-rcnn/
 # --------------------------------------------------------
 
+
+import os
+import sys
+os.environ['PYTHONUNBUFFERED'] = '1'
+os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
+os.environ['MXNET_ENABLE_GPU_P2P'] = '0'
+#os.environ['MXNET_ENGINE_TYPE'] = 'NaiveEngine'
+this_dir = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(this_dir, '..', '..', 'relation_rcnn'))
+
+
 import argparse
 import logging
 import pprint
