66110bd3990b7438991504073dbb54f7161a6b94
insigh <zhangchaojie@ruc.edu.cn>
2019-04-04 09:31:35 +0800

    debug

--- relation_rcnn/core/module.py
+++ relation_rcnn/core/module.py
@@ -272,7 +272,7 @@ class Module(BaseModule):
                         try:
                             cache_arr.copyto(arr)
                         except Exception as e:
-                            print ('Error:', name, cache_arr, arr.shape)
+                            print ('Error:', name, cache_arr.shape, arr.shape)
                 else:
                     if not allow_missing:
                         raise RuntimeError("%s is not presented" % name)
