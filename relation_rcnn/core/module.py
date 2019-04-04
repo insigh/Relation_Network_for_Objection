58d8aeddd2c1fd0075e217dc2ec22b12de047f35
insigh <zhangchaojie@ruc.edu.cn>
2019-04-05 00:53:19 +0800

    fpn training

--- relation_rcnn/core/module.py
+++ relation_rcnn/core/module.py
@@ -1027,12 +1027,12 @@ class MutableModule(BaseModule):
 
             # one epoch of training is finished
             for name, val in eval_metric.get_name_value():
-                self.logger.info('Epoch[%d] Train-%s=%f', epoch, name, val)
+                self.logger.info('Epoch[%d] Train-%s=%f'%(epoch, name, val))
 
                 toc = time.time()
-                self.logger.info('Epoch[%d] Time cost=%.3f', epoch, (toc-tic))
+                self.logger.info('Epoch[%d] Time cost=%.3f'%(epoch, (toc-tic)))
 
-                print('Epoch[%d] Time cost=%.3f', epoch, (toc-tic))
+                print('Epoch[%d] Time cost=%.3f'%(epoch, (toc-tic)))
 
             # sync aux params across devices
             arg_params, aux_params = self.get_params()
