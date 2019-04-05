5f3741dce4d4f71cde87ac26c500ed07249f711b
insigh <zhangchaojie@ruc.edu.cn>
2019-04-05 17:48:31 +0800

    fpn training

--- relation_rcnn/core/module.py
+++ relation_rcnn/core/module.py
@@ -1032,7 +1032,7 @@ class MutableModule(BaseModule):
                 toc = time.time()
                 self.logger.info('Epoch[%d] Time cost=%.3f'%(epoch, (toc-tic)))
 
-                print('Epoch[%d] Time cost=%.3f'%(epoch, (toc-tic)))
+            print('Epoch[%d] Time cost=%.3f'%(epoch, (toc-tic)))
 
             # sync aux params across devices
             arg_params, aux_params = self.get_params()
